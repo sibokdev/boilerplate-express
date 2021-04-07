d6b61eaa2b1c6b4636c406afbbf248da38f66e8e
sibokdev <>
2021-04-07 15:02:11 +0000

    return an html. file  with sendFile

--- myApp.js
+++ myApp.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send("Hello Express");
+});*/
+
+app.get("/", (req, res) => {
+  absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath)
 });
+
+
 
 
 
